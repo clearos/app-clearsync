4c8d105b2542f5c0a6d11a531da44bb4632fc9cc
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-03-18 18:46:51 +0000

    - Added Synchronization and Events app [tracker #493]

---
+++ deploy/info.php
@@ -0,0 +1,37 @@
+<?php
+
+/////////////////////////////////////////////////////////////////////////////
+// General information
+/////////////////////////////////////////////////////////////////////////////
+
+$app['basename'] = 'clearsync';
+$app['version'] = '1.0.9';
+$app['release'] = '1';
+$app['vendor'] = 'ClearFoundation';
+$app['packager'] = 'ClearFoundation';
+$app['license'] = 'GPLv3';
+$app['license_core'] = 'LGPLv3';
+$app['description'] = lang('clearsync_app_description');
+
+/////////////////////////////////////////////////////////////////////////////
+// App name and categories
+/////////////////////////////////////////////////////////////////////////////
+
+$app['name'] = lang('clearsync_app_name');
+$app['category'] = lang('base_category_system');
+$app['subcategory'] = lang('base_subcategory_settings');
+$app['menu_enabled'] = FALSE;
+
+/////////////////////////////////////////////////////////////////////////////
+// Packaging
+/////////////////////////////////////////////////////////////////////////////
+
+$app['core_only'] = TRUE;
+
+$app['core_requires'] = array(
+    'clearsync',
+);
+
+$app['core_file_manifest'] = array(
+    'clearsyncd.php'=> array('target' => '/var/clearos/base/daemon/clearsyncd.php'),
+);
---
+++ language/en_US/clearsync_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['clearsync_app_name'] = 'Synchronization and Events';
+$lang['clearsync_app_description'] = 'The Synchronization and Events engine provides hooks into to operating system for creating system events and synchronizing files across multiple systems.';
